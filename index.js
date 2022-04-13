ae1c2c7ccd5e1d807254995ee3bc994c0d463f29
Chris Rowe <rowe2ry@yahoo.com>
2022-04-13 16:26:23 -0400

    Began populating array of question objects for the inquire npm

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'Please enter the name for this project:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a brief description:'
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What was your motivation for this project?'
+    },
+    {
+        type: 'input',
+        name: 'build',
+        message: 'Why did you build this project?'
+    },
+    {
+        type: 'input',
+        name: 'solves',
+        message: 'What problem does it solve?'
+    },
+    {
+        type: 'input',
+        name: 'learned',
+        message: 'Talk about what you learned while building this application.'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How (if applicable) do you install this application?'
+    },
+    {
+        
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
